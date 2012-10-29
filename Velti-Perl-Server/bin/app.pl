e9897360be8d2f5fae9ebd81e4817001265be22b
Judioo <git@Judioo.com>
2012-10-29 10:06:02 +0000

    implemented support for displaying all campaigns

--- Velti-Perl-Server/bin/app.pl
+++ Velti-Perl-Server/bin/app.pl
@@ -1,5 +1,26 @@
 #!/usr/bin/env perl
 use Dancer;
 use Velti;
+use Dancer::Plugin::DBIC 'schema';
+
+
+get '/homepage' => sub {
+  # we can DBIx::Class to request a distinct list of
+  # all our campaigns
+  my @campaigns = schema->resultset('Vote')->search(
+    {
+    },
+    {
+      distinct  => 1,
+      columns   => [qw/campaign/],
+    }
+  )->all;
+
+  # display in our homepage template
+  template homepage => {
+    campaigns => \@campaigns
+  };
+};
+
 
 dance;
